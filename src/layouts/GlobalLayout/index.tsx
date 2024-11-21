67fa4216b0c0a222157dba9d6ebdcb709b85e372
june960427 <ljh975896@gmail.com>
2024-11-21 14:58:21 +0900

    ♻️ Refactor :  레이아웃 내 카드 삭제

--- src/layouts/GlobalLayout/index.tsx
+++ src/layouts/GlobalLayout/index.tsx
@@ -2,14 +2,12 @@ import Header from '@layouts/Header'
 import GlobalNav from '@layouts/GlobalNav'
 import { Outlet } from 'react-router-dom'
 import { GlobalContainer } from '@styles/globalStyle'
-import MateCard from '@components/MateCard'
 
 const GlobalLayout = () => {
   return (
     <div>
       <Header />
       <GlobalContainer>
-        <MateCard />
         <Outlet />
       </GlobalContainer>
       <GlobalNav />
