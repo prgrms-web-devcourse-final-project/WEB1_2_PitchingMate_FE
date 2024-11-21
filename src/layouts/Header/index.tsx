d52e31639397f48f2976a4649d3f9424387bc64b
w-ho-choo <ettoprivate@gmail.com>
2024-11-21 14:31:17 +0900

    ✨ Feat : 알림 아이콘 생성 #2

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -3,8 +3,11 @@ import { Link } from 'react-router-dom'
 import Logo from '/public/PITCHING MATE.svg?react'
 import Alarm from '@assets/icon/alarm.svg?react'
 
-import { AlarmWrap, HeaderBox, LogoWrap } from './style'
+import { AlarmWrap, HeaderBox, LogoWrap, NewAlarmPoint } from './style'
+import { useState } from 'react'
 const Header = () => {
+  const [newAlarmStatus, setNewAlarmStatus] = useState(true)
+
   return (
     <HeaderBox>
       <LogoWrap>
@@ -14,6 +17,7 @@ const Header = () => {
       </LogoWrap>
       <AlarmWrap>
         <Link to='/'>
+          {newAlarmStatus ? <NewAlarmPoint /> : null}
           <Alarm />
         </Link>
       </AlarmWrap>
