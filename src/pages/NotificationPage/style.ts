6281c5a677f09fb855ad6d38a17807397bac551e
Builter <builter251@gmail.com>
2024-11-25 16:30:38 +0900

    ✨ 알림 페이지 마크업 완료 #11

---
+++ src/pages/NotificationPage/style.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+import { theme } from '@styles/theme'
+
+export const NotificationPageContainer = styled.div``
+
+export const PillButtonContainer = styled.div`
+  margin: 0.5em 1.25em;
+`
+
+export const NotificationList = styled.ul`
+  list-style: none;
+
+  li {
+    display: flex;
+    align-items: flex-start;
+    padding: 1em 1.25em;
+    border-bottom: 1px solid ${theme.fontColor.cwhite};
+    position: relative;
+    background-color: ${theme.fontColor.white}; /* 기본 배경색 */
+
+    &:hover {
+      background-color: #f5f5f5;
+    }
+
+    &.read {
+      background-color: ${theme.fontColor.cwhite}; /* 읽은 알림 배경색 */
+    }
+
+    .new-badge {
+      width: 4px;
+      height: 4px;
+      border-radius: 50%;
+      background-color: red;
+      position: absolute;
+      top: 10px;
+      left: 10px;
+    }
+
+    .content {
+      .message {
+        font-size: ${theme.fontSize.medium};
+        font-weight: ${theme.fontWeight.regular};
+        margin: 0;
+      }
+
+      .time {
+        font-size: ${theme.fontSize.small};
+        color: ${theme.fontColor.black}66;
+        margin: 4px 0 0;
+      }
+    }
+  }
+`
