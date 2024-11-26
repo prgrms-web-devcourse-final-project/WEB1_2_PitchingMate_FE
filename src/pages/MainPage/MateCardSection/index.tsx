b10c29cb1cf7229134f92098e6172ea647e6b989
Builter <builter251@gmail.com>
2024-11-26 17:03:09 +0900

    ✨ Feat : 메이트 찾기 섹션 추가

--- src/pages/MainPage/MateCardSection/index.tsx
+++ src/pages/MainPage/MateCardSection/index.tsx
@@ -1,5 +1,21 @@
+import MateCard from '@components/MateCard'
+import { kboTeamInfo } from '@utils/kboInfo'
+import { MateCardContainer } from './style'
+import { useNavigate } from 'react-router-dom'
+
 const MateCardSection = () => {
-  return <div>MateCardSection</div>
+  const navigate = useNavigate()
+  return (
+    <MateCardContainer>
+      <h3>{`${kboTeamInfo.삼성.team} 메이트 찾기`}</h3>
+      <MateCard />
+      <MateCard />
+      <MateCard />
+      <p className='more' onClick={() => navigate('/')}>
+        더보기
+      </p>
+    </MateCardContainer>
+  )
 }
 
 export default MateCardSection
