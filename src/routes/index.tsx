f1bea42d59203b366fd70d1e09e9acc19a9d206c
Builter <builter251@gmail.com>
2024-11-26 12:33:36 +0900

    ✨ Feat : 로그인 페이지 마크업 완료

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,8 @@ import SubLayout from '@layouts/SubLayout'
 import ErrorPage from '@pages/ErrorPage'
 import MatePostingPage from '@pages/MatePostingPage'
 import { Routes, Route } from 'react-router-dom'
+import LoginPage from '@pages/LoginPage'
+import SignupPage from '@pages/LoginPage/SignupPage'
 
 const AppRoutes = () => {
   return (
@@ -13,6 +15,14 @@ const AppRoutes = () => {
         <Route
           path='/goods'
           element={<GoodsCard />}
+        />
+        <Route
+          path='/login'
+          element={<LoginPage />}
+        />
+        <Route
+          path='/login/signup'
+          element={<SignupPage />}
         />
       </Route>
       <Route element={<SubLayout />}>
