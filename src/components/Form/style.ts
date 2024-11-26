2a70e6cc651fa572ad10a9707acd60fdd81a61e3
Builter <builter251@gmail.com>
2024-11-26 11:41:52 +0900

    💄 Design : 입력폼 테두리 변경

--- src/components/Form/style.ts
+++ src/components/Form/style.ts
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
-import { theme } from '@styles/theme';
+import styled from 'styled-components'
+import { theme } from '@styles/theme'
 
 export const StyledForm = styled.form`
   display: flex;
@@ -8,29 +8,30 @@ export const StyledForm = styled.form`
   max-width: 480px;
   min-width: 350px;
   padding: 1em 1.25em;
-`;
+`
 
 export const StyledInputWrapper = styled.div`
   display: flex;
   flex-direction: column;
-`;
+  margin-bottom: 1em;
+`
 
 export const StyledLabel = styled.label`
   margin-bottom: 8px;
   font-size: ${theme.fontSize.xlarge};
   font-weight: ${theme.fontWeight.semi};
   color: ${theme.fontColor.black};
-`;
+`
 
 export const StyledInput = styled.input`
   box-sizing: border-box;
   padding: 1em;
   font-size: ${theme.fontSize.large};
-  border: 2px solid ${theme.border};
+  border: 2px solid ${theme.fontColor.cwhite};
   border-radius: 4px;
   transition: border-color 0.3s;
 
   &:focus {
     border: 2px solid ${theme.teams.kbo};
   }
-`;
+`
